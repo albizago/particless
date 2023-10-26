21c73c6e3eb298d7f3e333ebcecf68db55f281b6
Matteo Mirabelli <mirabellimatteo@gmail.com>
2023-10-26 10:23:13 +0200

    Particles v.1

--- particle.cpp
+++ particle.cpp
@@ -1,1 +1,32 @@
 #include "particle.hpp"
+
+const int pt::Particle::fMaxNumParticleType = 10;
+int pt::Particle::fNParticleType = 0;
+
+pt::Particle::Particle(char* name, Impulse P) : fIndex(FindParticle(name)), fP(P) {}
+
+
+const int pt::Particle::FindParticle(char* name) const {
+  int i{0};
+
+  for (;i < fMaxNumParticleType; ++i) {
+    if (fParticleType[i].getName() == name) {
+      break;
+    }
+  }
+
+  if (i != fMaxNumParticleType) {
+    return i;
+  } else {
+    throw std::runtime_error("Particle not found");
+  }
+}
+
+
+const int pt::Particle::getIndex() const {
+  return fIndex;
+}
+
+static void AddParticleType(char* name, double mass, int charge,
+                              double lenght);
+
--- particle.hpp
+++ particle.hpp
@@ -1,4 +1,35 @@
 #ifndef PARTICLE_HPP
 #define PARTICLE_HPP
 
+#include "particleType.hpp"
+
+struct Impulse {
+  double fPx{0};
+  double fPy{0};
+  double fPz{0};
+};
+
+namespace pt {
+
+class Particle {
+ public:
+  Particle(char* name, Impulse P);
+  const int getIndex() const;
+  static void AddParticleType(char* name, double mass, int charge,
+                              double lenght);
+  void setIndex(int index);
+  void setIndex(char* name);
+
+ private:
+  static ParticleType* fParticleType;
+  static const int fMaxNumParticleType;
+  static int fNParticleType;
+  const int fIndex;
+  Impulse fP;
+
+  const int FindParticle(char* name) const;
+};
+
+}  // namespace pt
+
 #endif
