e060a4c3e741a36e999abb4a7e5395dcebff128f
Matteo Mirabelli <mirabellimatteo@gmail.com>
2023-11-09 23:08:51 +0100

    comments and reorganization of couts

--- analyze_histos.C
+++ analyze_histos.C
@@ -15,6 +15,58 @@ void setStyle() {
   gStyle->SetOptTitle(0);
 }
 
+void analyze_particle_type(TH1I* histo) {
+  std::cout << "Analysing particle types\n";
+  std::cout << "Entries: " << histo->GetBinContent(1) << " +- "
+            << histo->GetBinError(1) << '\n';
+  ((histo->GetBinContent(1) - histo->GetBinError(1)) < 0.4 * 1.e7 &&
+   0.4 * 1.e7 < (histo->GetBinContent(1) + histo->GetBinError(1)))
+      ? std::cout << "Va bene\n\n"
+      : std::cout << "Scossa\n\n";
+
+  std::cout << "Entries: " << histo->GetBinContent(2) << " +- "
+            << histo->GetBinError(2) << '\n';
+  ((histo->GetBinContent(2) - histo->GetBinError(2)) < 0.4 * 1.e7 &&
+   0.4 * 1.e7 < (histo->GetBinContent(2) + histo->GetBinError(2)))
+      ? std::cout << "Va bene\n\n"
+      : std::cout << "Scossa\n\n";
+
+  std::cout << "Entries: " << histo->GetBinContent(3) << " +- "
+            << histo->GetBinError(3) << '\n';
+  (histo->GetBinContent(3) - histo->GetBinError(3) < 0.05 * 1.e7 &&
+   0.05 * 1.e7 < histo->GetBinContent(3) + histo->GetBinError(3))
+      ? std::cout << "Va bene\n\n"
+      : std::cout << "Scossa\n\n";
+
+  std::cout << "Entries: " << histo->GetBinContent(4) << " +- "
+            << histo->GetBinError(4) << '\n';
+  (histo->GetBinContent(4) - histo->GetBinError(4) < 0.05 * 1.e7 &&
+   0.05 * 1.e7 < histo->GetBinContent(4) + histo->GetBinError(4))
+      ? std::cout << "Va bene\n\n"
+      : std::cout << "Scossa\n\n";
+
+  std::cout << "Entries: " << histo->GetBinContent(5) << " +- "
+            << histo->GetBinError(5) << '\n';
+  (histo->GetBinContent(5) - histo->GetBinError(5) < 0.045 * 1.e7 &&
+   0.045 * 1.e7 < histo->GetBinContent(5) + histo->GetBinError(5))
+      ? std::cout << "Va bene\n\n"
+      : std::cout << "Scossa\n\n";
+
+  std::cout << "Entries: " << histo->GetBinContent(6) << " +- "
+            << histo->GetBinError(6) << '\n';
+  (histo->GetBinContent(6) - histo->GetBinError(6) < 0.045 * 1.e7 &&
+   0.045 * 1.e7 < histo->GetBinContent(6) + histo->GetBinError(6))
+      ? std::cout << "Va bene\n\n"
+      : std::cout << "Scossa\n\n";
+
+  std::cout << "Entries: " << histo->GetBinContent(7) << " +- "
+            << histo->GetBinError(7) << '\n';
+  (histo->GetBinContent(7) - histo->GetBinError(7) < 0.01 * 1.e7 &&
+   0.01 * 1.e7 < histo->GetBinContent(7) + histo->GetBinError(7))
+      ? std::cout << "Va bene\n\n"
+      : std::cout << "Scossa\n\n";
+}
+
 void analyze_histos() {
   TFile* histos_file = new TFile("histos.root", "read");
   bool compliance = true;
@@ -22,91 +74,105 @@ void analyze_histos() {
 
   TH1I* type = histos_file->Get<TH1I>("type");
 
-  ((type->GetBinContent(1) - type->GetBinError(1)) < 0.4 * 1.e7 &&
-   0.4 * 1.e7 < (type->GetBinContent(1) + type->GetBinError(1)))
-      ? std::cout << "Va bene\n"
-      : std::cout << "Scossa\n";
-  std::cout << type->GetBinContent(1) << " " << type->GetBinError(1) << '\n';
+  analyze_particle_type(type);
 
-  ((type->GetBinContent(2) - type->GetBinError(2)) < 0.4 * 1.e7 &&
-   0.4 * 1.e7 < (type->GetBinContent(2) + type->GetBinError(2)))
-      ? std::cout << "Va bene\n"
-      : std::cout << "Scossa\n";
-  std::cout << type->GetBinContent(2) << " " << type->GetBinError(2) << '\n';
+  /* ((type->GetBinContent(1) - type->GetBinError(1)) < 0.4 * 1.e7 &&
+    0.4 * 1.e7 < (type->GetBinContent(1) + type->GetBinError(1)))
+       ? std::cout << "Va bene\n"
+       : std::cout << "Scossa\n";
+   std::cout << type->GetBinContent(1) << " " << type->GetBinError(1) << '\n';
 
-  (type->GetBinContent(3) - type->GetBinError(3) < 0.05 * 1.e7 &&
-   0.05 * 1.e7 < type->GetBinContent(3) + type->GetBinError(3))
-      ? std::cout << "Va bene\n"
-      : std::cout << "Scossa\n";
-  std::cout << type->GetBinContent(3) << " " << type->GetBinError(3) << '\n';
+   ((type->GetBinContent(2) - type->GetBinError(2)) < 0.4 * 1.e7 &&
+    0.4 * 1.e7 < (type->GetBinContent(2) + type->GetBinError(2)))
+       ? std::cout << "Va bene\n"
+       : std::cout << "Scossa\n";
+   std::cout << type->GetBinContent(2) << " " << type->GetBinError(2) << '\n';
 
-  (type->GetBinContent(4) - type->GetBinError(4) < 0.05 * 1.e7 &&
-   0.05 * 1.e7 < type->GetBinContent(4) + type->GetBinError(4))
-      ? std::cout << "Va bene\n"
-      : std::cout << "Scossa\n";
-  std::cout << type->GetBinContent(4) << " " << type->GetBinError(4) << '\n';
+   (type->GetBinContent(3) - type->GetBinError(3) < 0.05 * 1.e7 &&
+    0.05 * 1.e7 < type->GetBinContent(3) + type->GetBinError(3))
+       ? std::cout << "Va bene\n"
+       : std::cout << "Scossa\n";
+   std::cout << type->GetBinContent(3) << " " << type->GetBinError(3) << '\n';
 
-  (type->GetBinContent(5) - type->GetBinError(5) < 0.045 * 1.e7 &&
-   0.045 * 1.e7 < type->GetBinContent(5) + type->GetBinError(5))
-      ? std::cout << "Va bene\n"
-      : std::cout << "Scossa\n";
-  std::cout << type->GetBinContent(5) << " " << type->GetBinError(5) << '\n';
+   (type->GetBinContent(4) - type->GetBinError(4) < 0.05 * 1.e7 &&
+    0.05 * 1.e7 < type->GetBinContent(4) + type->GetBinError(4))
+       ? std::cout << "Va bene\n"
+       : std::cout << "Scossa\n";
+   std::cout << type->GetBinContent(4) << " " << type->GetBinError(4) << '\n';
 
-  (type->GetBinContent(6) - type->GetBinError(6) < 0.045 * 1.e7 &&
-   0.045 * 1.e7 < type->GetBinContent(6) + type->GetBinError(6))
-      ? std::cout << "Va bene\n"
-      : std::cout << "Scossa\n";
-  std::cout << type->GetBinContent(6) << " " << type->GetBinError(6) << '\n';
+   (type->GetBinContent(5) - type->GetBinError(5) < 0.045 * 1.e7 &&
+    0.045 * 1.e7 < type->GetBinContent(5) + type->GetBinError(5))
+       ? std::cout << "Va bene\n"
+       : std::cout << "Scossa\n";
+   std::cout << type->GetBinContent(5) << " " << type->GetBinError(5) << '\n';
 
-  (type->GetBinContent(7) - type->GetBinError(7) < 0.01 * 1.e7 &&
-   0.01 * 1.e7 < type->GetBinContent(7) + type->GetBinError(7))
-      ? std::cout << "Va bene\n"
-      : std::cout << "Scossa\n";
-  std::cout << type->GetBinContent(7) << " " << type->GetBinError(7) << '\n';
+   (type->GetBinContent(6) - type->GetBinError(6) < 0.045 * 1.e7 &&
+    0.045 * 1.e7 < type->GetBinContent(6) + type->GetBinError(6))
+       ? std::cout << "Va bene\n"
+       : std::cout << "Scossa\n";
+   std::cout << type->GetBinContent(6) << " " << type->GetBinError(6) << '\n';
+
+   (type->GetBinContent(7) - type->GetBinError(7) < 0.01 * 1.e7 &&
+    0.01 * 1.e7 < type->GetBinContent(7) + type->GetBinError(7))
+       ? std::cout << "Va bene\n"
+       : std::cout << "Scossa\n";
+   std::cout << type->GetBinContent(7) << " " << type->GetBinError(7) << '\n';
+ */
 
   TH2D* angle_histo = histos_file->Get<TH2D>("angles");
   TF1* uniform_dist = new TF1("unif", "[0]", 0., 2 * TMath::Pi());
+
   angle_histo->ProjectionX()->Fit(uniform_dist, "q", "", 0., 2 * TMath::Pi());
 
-  std::cout << "\n PHI FIT FUNCTION\n";
-  std::cout << "\n Parameter: " << uniform_dist->GetParameter(0);
+  std::cout << "\n AZIMUTHAL ANGLE PHI -- FIT FUNCTION\n";
+  std::cout << "\n Parameter (normalization): " << uniform_dist->GetParameter(0)
+            << " +- " << uniform_dist->GetParError(0);
   std::cout << "\n Chi square: " << uniform_dist->GetChisquare();
   std::cout << "\n NDF: " << uniform_dist->GetNDF();
   std::cout << "\n Chi/NDF: "
             << uniform_dist->GetChisquare() / uniform_dist->GetNDF();
   std::cout << "\n Probability: " << uniform_dist->GetProb();
+  std::cout << "--------------------- \n\n";
 
   uniform_dist->SetRange(0., TMath::Pi());
   angle_histo->ProjectionY()->Fit(uniform_dist, "q", "", 0., TMath::Pi());
 
-  std::cout << "\n THETA FIT FUNCTION\n";
-  std::cout << "\n Parameter: " << uniform_dist->GetParameter(0);
+  std::cout << "\n POLAR ANGLE THETA -- FIT FUNCTION\n";
+  std::cout << "\n Parameter (normalization): " << uniform_dist->GetParameter(0)
+            << " +- " << uniform_dist->GetParError(0);
   std::cout << "\n Chi square: " << uniform_dist->GetChisquare();
   std::cout << "\n NDF: " << uniform_dist->GetNDF();
   std::cout << "\n Chi/NDF: "
             << uniform_dist->GetChisquare() / uniform_dist->GetNDF();
-  std::cout << "\n Probability: " << uniform_dist->GetProb();
+  std::cout << "\n Probability: " << uniform_dist->GetProb() << '\n';
+  std::cout << "--------------------- \n\n";
+
+  // Retrieving impulse histogram
 
   TH1D* impulse_histo = histos_file->Get<TH1D>("impulse");
-
   TF1* exp_dist = new TF1("exp", "expo([0], [1])", 0., 30.);
 
   impulse_histo->Fit("exp", "q", "", 0., 30.);
 
   TF1* fit_func = impulse_histo->GetFunction("exp");
 
-  std::cout << "\n IMPULSE FIT FUNCTION\n";
-  std::cout << "\n Parameter 0: " << fit_func->GetParameter(0);
-  std::cout << "\n Parameter 1: " << fit_func->GetParameter(1);
+  std::cout << "\n IMPULSE -- FIT FUNCTION\n";
+  std::cout << "\n Parameter 0 (normalization): " << fit_func->GetParameter(0)
+            << " +- " << fit_func->GetParError(0);
+  std::cout << "\n Parameter 1 (average): " << fit_func->GetParameter(1)
+            << " +- " << fit_func->GetParError(1);
   std::cout << "\n Chi square: " << fit_func->GetChisquare();
   std::cout << "\n NDF: " << fit_func->GetNDF();
   std::cout << "\n Chi/NDF: " << fit_func->GetChisquare() / fit_func->GetNDF();
-  std::cout << "\n Probability: " << fit_func->GetProb();
+  std::cout << "\n Probability: " << fit_func->GetProb() << '\n';
+  std::cout << "--------------------- \n\n";
 
   (impulse_histo->GetMean() - impulse_histo->GetMeanError() < 1. &&
    1. < impulse_histo->GetMean() + impulse_histo->GetMeanError())
       ? std::cout << "\n Va bene lo stessoooo \n"
       : std::cout << "\n Problemi con exp \n";
+
+  // Retrieving invariant masses histograms
 
   TH1D* disc_histo = histos_file->Get<TH1D>("inv_mass_disc");
   TH1D* conc_histo = histos_file->Get<TH1D>("inv_mass_conc");
@@ -118,12 +184,18 @@ void analyze_histos() {
   canva->Divide(1, 2);
   canva->cd(1);
 
+  // Difference between invariant masses of all particles of opposite charge and
+  // all particles of same charge
+
   TH1D* diff1_histo = new TH1D(*disc_histo);
   diff1_histo->Sumw2();
   diff1_histo->Add(disc_histo, conc_histo, 1., -1.);
 
   diff1_histo->Draw();
   canva->cd(2);
+
+  // Difference between invariant masses of pions and kaons of opposite charges
+  // and of same charges
 
   TH1D* diff2_histo = new TH1D(*disc_pk_histo);
   diff2_histo->Sumw2();
@@ -132,8 +204,9 @@ void analyze_histos() {
   diff2_histo->Draw();
   canva->cd(1);
 
-  TF1* gauss_dist = new TF1("gauss", "gaus([0], [1], [2])", 0., 7.5);
+  // Fitting with gaussian distribution
 
+  TF1* gauss_dist = new TF1("gauss", "gaus([0], [1], [2])", 0., 7.5);
   gauss_dist->SetParameters(10, 0.8, 0.05);
 
   diff1_histo->Fit("gauss", "q", "", 0., 7.5);
@@ -142,28 +215,36 @@ void analyze_histos() {
   fit_func->Draw("same");
   canva->cd(2);
 
-  std::cout << "\n DIFFERENCE 1 FIT FUNCTION\n";
-  std::cout << "\n Parameter 0: " << fit_func->GetParameter(0);
-  std::cout << "\n Parameter 1: " << fit_func->GetParameter(1);
-  std::cout << "\n Parameter 2: " << fit_func->GetParameter(2);
+  std::cout << "\n ALL PARTICLES DIFFERENCE -- GAUSSIAN FIT FUNCTION\n";
+  std::cout << "\n Parameter 0: " << fit_func->GetParameter(0) << " +- "
+            << fit_func->GetParError(0);
+  std::cout << "\n Parameter 1 (average): " << fit_func->GetParameter(1)
+            << " +- " << fit_func->GetParError(1);
+  std::cout << "\n Parameter 2 (RMS): " << fit_func->GetParameter(2) << " +- "
+            << fit_func->GetParError(2);
   std::cout << "\n Chi square: " << fit_func->GetChisquare();
   std::cout << "\n NDF: " << fit_func->GetNDF();
   std::cout << "\n Chi/NDF: " << fit_func->GetChisquare() / fit_func->GetNDF();
   std::cout << "\n Probability: " << fit_func->GetProb() << '\n';
+  std::cout << "--------------------- \n\n";
 
   diff2_histo->Fit("gauss", "q", "", 0., 7.5);
 
   fit_func = diff2_histo->GetFunction("gauss");
   fit_func->Draw("same");
 
-  std::cout << "\n DIFFERENCE 2 FIT FUNCTION\n";
-  std::cout << "\n Parameter 0: " << fit_func->GetParameter(0);
-  std::cout << "\n Parameter 1: " << fit_func->GetParameter(1);
-  std::cout << "\n Parameter 2: " << fit_func->GetParameter(2);
+  std::cout << "\n PIONS & KAONS DIFFERENCE -- GAUSSIAN FIT FUNCTION\n";
+  std::cout << "\n Parameter 0: " << fit_func->GetParameter(0) << " +- "
+            << fit_func->GetParError(0);
+  std::cout << "\n Parameter 1 (average): " << fit_func->GetParameter(1)
+            << " +- " << fit_func->GetParError(1);
+  std::cout << "\n Parameter 2 (RMS): " << fit_func->GetParameter(2) << " +- "
+            << fit_func->GetParError(2);
   std::cout << "\n Chi square: " << fit_func->GetChisquare();
   std::cout << "\n NDF: " << fit_func->GetNDF();
   std::cout << "\n Chi/NDF: " << fit_func->GetChisquare() / fit_func->GetNDF();
   std::cout << "\n Probability: " << fit_func->GetProb() << '\n';
+  std::cout << "--------------------- \n\n";
 
   // TFile* data_out = new TFile("data.txt", "recreate");
 }
